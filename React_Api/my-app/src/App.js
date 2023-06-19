a0868452bdd1880df2d5c768251d748309c4db84
Falalatina <karolina40.gorska17@gmail.com>
2023-06-19 14:45:50 +0200

    Wyswietlanie 1 raz
    juz ladnie pobiera przy kazdym odswiezeniu

--- React_Api/my-app/src/App.js
+++ React_Api/my-app/src/App.js
@@ -1,25 +1,30 @@
 
 import './App.css';
-import Axios from Axios;
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
-fetch("https://catfact.ninja/fact")
-.then((res)=> res.json())
-.then((data)=>{
-  console.log(data);
-}
-);
+// fetch("https://catfact.ninja/fact")
+// .then((res)=> res.json())
+// .then((data)=>{
+//   console.log(data);
+// }
+// );
 
-Axios.get("https://catfact.ninja/fact")
-.then((res) => res.json())
-.then((data) =>{
-  console.log(data);
-});
 
 function App() {
+const [catFact, setCatFact] = useState("");
+
+useEffect(()=>{
+  Axios.get("https://catfact.ninja/fact").then((res) => {
+    setCatFact(res.data.fact);
+  });
+}, []);
+
+
   return(
     <div className="App">
       <button>Generate Cat facts</button>
-      <p> </p>
+      <p> {catFact} </p>
       </div>
   );
 
